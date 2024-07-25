744d54149afc61ca4e25831aa2a54b4396228621
nainsworth <nja8161995@gmail.com>
2024-07-25 01:10:25 -0500

    Initial Commit

---
+++ src/JS/utils.js
@@ -0,0 +1,5 @@
+const convertTemp = (temp) => {
+  return (temp * (9 / 5) + 32).toFixed(1);
+};
+
+export { convertTemp };
---
+++ src/JS/weather.js
@@ -0,0 +1,21 @@
+// import fetchData from "./api.js";
+import { convertTemp } from "./utils.js";
+
+const loadWeather = (data) => {
+  const currentWeather = document.querySelector(".current-weather-container");
+
+  const location = document.createElement("h2");
+  location.textContent = `${data.resolvedAddress}`;
+
+  const currentTemp = document.createElement("h1");
+  currentTemp.textContent = `${convertTemp(data.currentConditions.temp)} °F`;
+
+  const currentCondition = document.createElement("p");
+  currentCondition.textContent = `${data.currentConditions.conditions}`;
+
+  currentWeather.appendChild(location);
+  currentWeather.appendChild(currentTemp);
+  currentWeather.appendChild(currentCondition);
+};
+
+export { loadWeather };
